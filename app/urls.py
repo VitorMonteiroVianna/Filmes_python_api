88f480d60ed493e186358225801c44cbe81f3446
VitorMonteiroVianna <vmonteiro2004@gmail.com>
2024-09-27 21:17:49 -0300

    Add get especifico, update e delete de genero

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import GenreCreateListView
+from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('genre/', GenreCreateListView.as_view(), name='create_list_view'),
+    path('genre/<int:pk>/', GenreRetrieveUpdateDestroyAPIView.as_view(), name='create_list_view'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -6,3 +6,8 @@ from genres.serializers import GenrerSerializer
 class GenreCreateListView(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenrerSerializer
+
+
+class GenreRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenrerSerializer
