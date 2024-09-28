07cb8bdbbe15cf53842f3e8078cc08e7ea1023aa
VitorMonteiroVianna <vmonteiro2004@gmail.com>
2024-09-27 21:41:31 -0300

    Add middleware docs in Genres

--- genres/views.py
+++ genres/views.py
@@ -4,10 +4,17 @@ from genres.serializers import GenrerSerializer
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
+    """
+    Endpoint reponsavel por listar os generos disposniveis com GET e adicionar novos com POST.
+    """
+    
     queryset = Genre.objects.all()
     serializer_class = GenrerSerializer
 
 
 class GenreRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Endpoint reponsavel por passar as informações de um genero especifico de acordo com seu ID. Pode fazer alterações nesse genero: use o PUT, PATCH para atualizar o endpont e DELETE para exclui-lo.
+    """
     queryset = Genre.objects.all()
     serializer_class = GenrerSerializer
