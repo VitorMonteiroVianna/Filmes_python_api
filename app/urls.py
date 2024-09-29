438eb785af91169422b2a5d349f6678deef2ace1
VitorMonteiroVianna <vmonteiro2004@gmail.com>
2024-09-29 00:27:47 -0300

    Redireciona o usuario para documentação

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
+from django.views.generic import RedirectView 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.urls import path, re_path
@@ -19,6 +20,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/redoc/', permanent=False)), #Faz com que ao bater na raiz do projeto, va para a documentação
     
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
